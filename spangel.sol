24441850f9602aff0326512821798a7ab1325630
b3rtL <xdaegonx@gmail.com>
2018-06-25 22:30:18 -0700

    added some modifiers

--- spangel.sol
+++ spangel.sol
@@ -21,28 +21,23 @@ contract Spangel {
 
   mapping(bytes32 => Begger) public beggers;
 
-  modifier isAlive(bytes32 _userName) {
-    require(beggers[_userName].alive == true);
+  modifier isAlive(bytes32 _uuid) {
+    require(beggers[_uuid].alive == true);
     _;
   }
 
-  modifier isOwner(bytes32 _userName) {
-    require(beggers[_userName].owner == msg.sender);
+  modifier isOwner(bytes32 _uuid) {
+    require(beggers[_uuid].owner == msg.sender);
     _;
   }
 
-  modifier isNew(bytes32 _userName) {
-    require(beggers[_userName].completed == false);
+  modifier isExpired(bytes32 _uuid) {
+    require(beggers[_uuid].ttl >= now);
     _;
   }
 
-  modifier isExpired(bytes32 _userName) {
-    require(beggers[_userName].ttl >= now);
-    _;
-  }
-
-  modifier isGiver(bytes32 _userName) {
-    require(beggers[_userName].givers[msg.sender] > 0);
+  modifier isGiver(bytes32 _uuid) {
+    require(beggers[_uuid].givers[msg.sender] > 0);
     _;
   }
 
@@ -52,46 +47,48 @@ contract Spangel {
     beggerCount = 0;
   }
 
-  function createBegger(bytes32 _userName,uint ttl, uint _projected) public isNew(_userName) {
-    beggers[_userName].completed = false;
-    beggers[_userName].coinRaised = 0;
-    beggers[_userName].ttl = SafeMath.add(ttl, now);
-    beggers[_userName].projected = _projected;
-    beggers[_userName].alive = true;
-    beggers[_userName].owner = msg.sender;
+  function createBegger(bytes32 _uuid, uint ttl, uint _projected) public {
+    require(beggers[_uuid].owner == address(0x0));
+    beggers[_uuid].completed = false;
+    beggers[_uuid].coinRaised = 0;
+    ttl = SafeMath.mul(ttl, 1 days);
+    beggers[_uuid].ttl = SafeMath.add(ttl, now);
+    beggers[_uuid].projected = _projected;
+    beggers[_uuid].alive = true;
+    beggers[_uuid].owner = msg.sender;
     beggerCount++;
   }
 
-  function addResolver(bytes32 _userName, address _resolver) public isOwner(_userName) isAlive(_userName) {
-    beggers[_userName].resolver = _resolver;
+  function addResolver(bytes32 _uuid, address _resolver) public isOwner(_uuid) isAlive(_uuid) {
+    beggers[_uuid].resolver = _resolver;
   }
 
-  function give(bytes32 _userName) public payable {
-    require(msg.value < beggers[_userName].projected);
-    beggers[_userName].coinRaised = SafeMath.add(beggers[_userName].coinRaised, SafeMath.div(msg.value, 200));
+  function give(bytes32 _uuid) public payable isAlive(_uuid) {
+    require(msg.value < beggers[_uuid].projected);
+    beggers[_uuid].coinRaised = SafeMath.add(beggers[_uuid].coinRaised, SafeMath.div(msg.value, 200));
     bank = SafeMath.add(bank, msg.value);
-    beggers[_userName].givers[msg.sender] = SafeMath.add(msg.value, beggers[_userName].givers[msg.sender]);
+    beggers[_uuid].givers[msg.sender] = SafeMath.add(msg.value, beggers[_uuid].givers[msg.sender]);
 
-    if (beggers[_userName].coinRaised >= beggers[_userName].projected) {
-      beggers[_userName].resolver.transfer(beggers[_userName].projected);
-      msg.sender.transfer(SafeMath.sub(beggers[_userName].coinRaised, beggers[_userName].projected));
-      bank = SafeMath.sub(bank, beggers[_userName].projected);
-      bank = SafeMath.sub(bank, SafeMath.sub(beggers[_userName].coinRaised, beggers[_userName].projected));
-      beggers[_userName].alive = false;
-      beggers[_userName].completed = true;
+    if (beggers[_uuid].coinRaised >= beggers[_uuid].projected) {
+      beggers[_uuid].resolver.transfer(beggers[_uuid].projected);
+      msg.sender.transfer(SafeMath.sub(beggers[_uuid].coinRaised, beggers[_uuid].projected));
+      bank = SafeMath.sub(bank, beggers[_uuid].projected);
+      bank = SafeMath.sub(bank, SafeMath.sub(beggers[_uuid].coinRaised, beggers[_uuid].projected));
+      beggers[_uuid].alive = false;
+      beggers[_uuid].completed = true;
     }
 
   }
 
-  function refund(bytes32 _userName) public isGiver(_userName) isExpired(_userName) {
+  function refund(bytes32 _uuid) public isGiver(_uuid) isExpired(_uuid) {
     uint owed;
-    owed = beggers[_userName].givers[msg.sender];
-    beggers[_userName].givers[msg.sender] = 0;
+    owed = beggers[_uuid].givers[msg.sender];
+    beggers[_uuid].givers[msg.sender] = 0;
     bank = SafeMath.sub(bank, owed);
-    beggers[_userName].coinRaised = SafeMath.sub(beggers[_userName].coinRaised, owed);
+    beggers[_uuid].coinRaised = SafeMath.sub(beggers[_uuid].coinRaised, owed);
     msg.sender.transfer(owed);
-    beggers[_userName].alive = false;
-    beggers[_userName].completed = true;
+    beggers[_uuid].alive = false;
+    beggers[_uuid].completed = true;
   }
 
 }
